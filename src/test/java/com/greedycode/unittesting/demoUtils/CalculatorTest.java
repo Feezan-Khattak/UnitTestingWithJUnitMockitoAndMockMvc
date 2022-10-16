c9acbdd5aed8232af5a6490f149cd20028741856
Feezan Khattak <feezanktk2208@gmail.com>
2022-10-16 13:49:14 +0500

    test cases life cycle

--- src/test/java/com/greedycode/unittesting/demoUtils/CalculatorTest.java
+++ src/test/java/com/greedycode/unittesting/demoUtils/CalculatorTest.java
@@ -1,24 +1,47 @@
 package com.greedycode.unittesting.demoUtils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    Calculator calculator;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        calculator = new Calculator();
+        System.out.println("Before each method called");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Tear down after each");
+    }
+
+    @BeforeAll
+    static void setupBeforeAll() {
+        System.out.println("Setup before all");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll() {
+        System.out.println("Tear Down after all");
+    }
+
     @Test
-    public void addTwoNumber_ShouldBeEqualOrUnEqual() {
+    void addTwoNumber_ShouldBeEqualOrUnEqual() {
         // setup
-        Calculator calculator = new Calculator();
         int expected = 7;
         int unexpected = 8;
 
         // execute
-        int result = calculator.add(3,  4);
+        int result = calculator.add(3, 4);
 
         // assert
         assertEquals(expected, result, "3+4 should be 7");
         assertNotEquals(unexpected, result, "3+4 should not be 8");
     }
+
 
 }
